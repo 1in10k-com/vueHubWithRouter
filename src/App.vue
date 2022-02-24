bab4362aece8d38094767c74299bffbe76d369e2
playdog-io <cqdxcs3@163.com>
2022-02-24 23:23:45 +0800

    f04，router-link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <router-link to="/" replace="">跳转到a</router-link>
+    <router-link to="/b" replace="">跳转到b</router-link>
+    <router-link to="/c" replace="">跳转到c</router-link>
     <router-view></router-view>
   </div>
 </template>
